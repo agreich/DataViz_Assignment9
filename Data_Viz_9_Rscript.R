e613ef22bdfbac35002aaa3565557c93f2bfdc96
Alexandra Reich <agreich@alaska.edu>
2021-03-31 14:18:25 -0800

    Made graphs, need to perfect them and write the captions, still

--- Data_Viz_9_Rscript.R
+++ Data_Viz_9_Rscript.R
@@ -25,13 +25,25 @@ pol.dat <- pol.dat %>% filter(!is.na(Age..years.),
 
 
 #plot one: sablefish length by weight
-g1 <- ggplot(data=sable) +aes(y=Weight..gm., x=Length..mm.) + geom_point() +theme_cowplot(14)+facet_wrap(~Sex) +theme(text=element_text(family="Times New Roman"))+
+ggplot(data=sable) +aes(y=Weight..gm., x=Length..mm.) + geom_point() +theme_cowplot(14)+facet_wrap(~Sex) +theme(text=element_text(family="Times New Roman"))+
   labs(x="Length(mm)", y="Weight(gm)")
+#save the figure
+dev.new (width=2.75, height=1.83, unit= "in", noRStudioGD = T); lastplot()
+ggsave("Assigment9_fig1.eps", width=dev.size()[1], height=dev.size()[2]);dev.off()
+#save as what file type?
+#eps(adobe illustratior), tiff, or psd(photoshop. I'll try tiff
+
 
 #plot two, count the number of species
-
-g2 <- gplot(data=goa)+aes(x=Common.Name)+ geom_bar() +
+ggplot(data=goa)+aes(x=Common.Name)+ geom_bar() +
   coord_flip()+
+  #scale_y_continuous(limits=c(0,14000), expand=expansion(mult =c(0.05,0)))+
   theme_cowplot(14) +theme(text=element_text(family="Times New Roman"))+
   labs(y = "Abundance", x="Species")
+#save the figure
+dev.new (width=2.75, height=1.83, unit= "in", noRStudioGD = T); lastplot()
+ggsave("Assigment9_fig2.eps", width=dev.size()[1], height=dev.size()[2]);dev.off()
 
+
+#to do:
+#expand x-and y-axes to encompass end of things
